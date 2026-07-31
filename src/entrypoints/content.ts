d4a9e7965bc7bd84e409124af72ffcdb9d49ad17
Gerard <289036782+splits-gerard@users.noreply.github.com>
2026-07-31 18:38:47 +0000

    fix: support HyperEVM

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -19,7 +19,7 @@ import {
   isSessionInfoMessage,
 } from "@/utils/session-info";
 import { Chains, Dialog, Mode, Porto } from "@splits/porto";
-import { robinhood, tempo, worldchain } from "viem/chains";
+import { hyperEvm, robinhood, tempo, worldchain } from "viem/chains";
 import { getAllowedOrigins, getHost, getRelay } from "../../utils";
 
 export default defineContentScript({
@@ -175,7 +175,7 @@ class ContentBridge {
     const providerInfo = getProviderInfo(import.meta.env.MODE);
     this.porto = Porto.create({
       announceProvider: providerInfo,
-      chains: [...Chains.all, worldchain, tempo, robinhood],
+      chains: [...Chains.all, worldchain, tempo, hyperEvm, robinhood],
       mode: Mode.dialog({
         host: `${getHost(import.meta.env.MODE)}/connect/`,
         renderer: Dialog.popup({
